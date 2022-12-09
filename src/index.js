bad10334cdf5b057f9da9f3a70cf97dff4f630bb
john smilga <johnsmilga@gmail.com>
2022-12-09 12:41:36 -0500

    switch to React 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Auth0Provider
       domain='dev-pu8wyk-g.us.auth0.com'
@@ -18,8 +18,7 @@ ReactDOM.render(
         <App />
       </GithubProvider>
     </Auth0Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
